2180ddf591bf729b08c709edb7161ce956eb3935
Mervyng <144014542+Mervyng@users.noreply.github.com>
2023-10-25 12:02:52 -0700

    Exercise 2.py

--- Chapter 3- Data Structures/Exercises/Exercise 2.py
+++ Chapter 3- Data Structures/Exercises/Exercise 2.py
@@ -1,3 +1,11 @@
+'''Start with the list you used in Exercise 1, but instead of just
+
+printing each person’s name, print a message to them. The text of each message should be the same, but each message should be 
+
+personalized with the person’s name.'''
+
+
+
 names = ['ron', 'tyler', 'dani']
 
 msg = f"Hello, {names[0].title()}!"
