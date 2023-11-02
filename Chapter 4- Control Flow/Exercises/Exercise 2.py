bd6192d84bc1f3e79bbd73c0f6d637d9ec2fd818
Mervyng <144014542+Mervyng@users.noreply.github.com>
2023-11-02 10:33:20 -0700

    Exercise 2.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise 2.py
@@ -0,0 +1,8 @@
+# If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+Alien_color ="red"
+
+if Alien_color =="red":
+    print("Congrats! you earned 5 points!")
+else:
+    print("Congrats! you earned 10 points!")
+        
