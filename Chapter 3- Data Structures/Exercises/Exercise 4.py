cb61bb86279374e2863f85e938977034d4e29ac7
Mervyng <144014542+Mervyng@users.noreply.github.com>
2023-11-13 23:43:20 -0800

    Update Exercise 4.py

--- Chapter 3- Data Structures/Exercises/Exercise 4.py
+++ Chapter 3- Data Structures/Exercises/Exercise 4.py
@@ -4,30 +4,30 @@
 
 
 # Invite some people to dinner.
-guests = ['guido van rossum', 'jack turner', 'lynn hill']
+guests = ['Guido van Rossum, 'Jack Turner', 'Lynn Hill']
 
 name = guests[0].title()
 print(f"{name}, please come to dinner.")
 
 name = guests[1].title()
-print(f"{name}, please come to dinner.")
+print(f"{name}, Hi sir, please come sit with us and have dinner.")
 
 name = guests[2].title()
-print(f"{name}, please come to dinner.")
+print(f"{name}, please come and eat.")
 
 name = guests[1].title()
-print(f"\nSorry, {name} can't make it to dinner.")
+print(f"\nSorry, {name} sorry I can't make it to dinner.")
 
 # Jack can't make it! Let's invite Gary instead.
 del(guests[1])
-guests.insert(1, 'gary snyder')
+guests.insert(1, 'Gary Snyder')
 
 # Print the invitations again.
 name = guests[0].title()
-print(f"\n{name}, please come to dinner.")
+print(f"\n{name}, sorry I can't come home for dinner.")
 
 name = guests[1].title()
-print(f"{name}, please come to dinner.")
+print(f"{name}, please come to the party dinner.")
 
 name = guests[2].title()
 print(f"{name}, please come to dinner.")
